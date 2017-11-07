23df55cd9106f32dbcdb54182f0b22aa334a85e6
Maryoku <maryoku.amaya@gmail.com>
2017-11-07 14:39:48 +0500

    fix one more BUG, AGAIN

--- controllers/kittenController.php
+++ controllers/kittenController.php
@@ -35,16 +35,16 @@ class kittenController extends Controller {
 		
 		$_PUT=json_decode(file_get_contents('php://input'), true);
 		
-		if( (isset($_POST['id'])) && (isset($_POST['name'])) && (isset($_POST['image'])) 
-			&& (isset($_POST['power'])) && (isset($_POST['speed'])) ) {
+		if( (isset($_PUT['id'])) && (isset($_PUT['name'])) && (isset($_PUT['image'])) 
+			&& (isset($_PUT['power'])) && (isset($_PUT['speed'])) ) {
 			// мы передаем в модель массив с данными
 			// модель должна вернуть boolean
 			$dataToUpd=array(
-					'id'=>$_POST['id'], 
-					'name'=>$_POST['name'],
-					'image'=>$_POST['image'],
-					'power'=>$_POST['power'],
-					'speed'=>$_POST['speed']);
+					'id'=>$_PUT['id'], 
+					'name'=>$_PUT['name'],
+					'image'=>$_PUT['image'],
+					'power'=>$_PUT['power'],
+					'speed'=>$_PUT['speed']);
 			$updItem=$this->model->save($data['id'], $dataToUpd);
 			$this->setResponce($updItem);
 			}
